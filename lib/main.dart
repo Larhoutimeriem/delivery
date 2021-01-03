769539a5813a17934a827eea4e8e59cd417368f0
Meriem Larhouti <meriem.larhouti@gmail.com>
2021-01-03 04:01:37 +0100

    location

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
 import 'Widgets/login.dart';
+import 'dart:async';
+import 'package:delivery/utils/locationJs.dart';
+import 'package:js/js.dart';
+import 'package:delivery/utils/authentication.dart';
+import 'package:firebase/firebase.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  Timer.periodic(new Duration(seconds: 10), (timer) {
+    _setCurrentLocation();
+  }); 
+  runApp(MyApp());
+}
+
+success(pos) async{
+  try {
+    print(pos.coords.latitude);
+    print(pos.coords.longitude);
+    await getUser();
+    print(uid);
+    if (uid != null){
+      Database db = database();
+      DatabaseReference ref = db.ref('livreur/'+uid);
+      ref.set({'longitude': pos.coords.longitude, 'latitude': pos.coords.latitude});
+    }
+  } catch (ex) {
+      print("Exception thrown : " + ex.toString());
+    }
+}
+
+_setCurrentLocation() {
+  getCurrentPosition(allowInterop((pos) => success(pos)));
+}
 
 class MyApp extends StatelessWidget {
   @override
